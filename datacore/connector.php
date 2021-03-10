f24518118542b2d3665c7c51de2cb66519cf68a8
Giulliano Rossi <giulliano.scatalon.rossi@gmail.com>
2021-03-10 13:36:40 +0000

    Started building database

---
+++ datacore/connector.php
@@ -0,0 +1,13 @@
+<?php
+namespace Connection;
+require_once "/workspace/yu-gi-discord/datacore/exceptions/connection.php";
+
+
+class Connection{
+    private $fileLoaded;
+    private $gotFile;
+
+    public function __construct(){
+        
+    }
+}
